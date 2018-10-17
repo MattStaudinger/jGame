2189029a8ba33a411a16ed188a218306836ce83c
Matthias Staudinger <staudinger.matthias@googlemail.com>
2018-10-17 18:06:04 +0200

    Add architecture of page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log("Hello")
+
+
+function goToPage(link) {
+
+  //Show page
+  $("[data-page=" + link + "]").siblings("[data-page]").hide()
+  $("[data-page=" + link + "]").show()
+
+    // Add the class active in the navbar
+    $('li.nav-item').each(function(){
+      var href = $(this).find('a.nav-link').attr('href')
+      if (href === link) 
+        $(this).addClass('active')
+      else
+        $(this).removeClass('active')
+})
+}
+
+goToPage("about");
+
+$("a").click(function(event) {
+  event.preventDefault();
+  goToPage($(this).attr("href"))
+})
